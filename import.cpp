847853d78fcbfeccbf8153eddde6f3c802d22a8b
galex <alex.gelineau49@gmail.com>
2022-04-26 14:35:47 +0200

    Création fonction distance

--- import.cpp
+++ import.cpp
@@ -1,7 +1,11 @@
 #include <fstream>
+#include <cmath>
 #include "import.h"
 
-double distance(point a, point b);
+double distance(point a, point b)
+{
+    return sqrt( pow(b.y - a.y , 2) + pow(b.x - a.x , 2))
+}
 
 void import_fic_cord(double **t, std::string file);
 
