23ba741e196075cc6eafa24a2cf2e2cc22c3aa5b
leg7 <leonard.gomez@protonmail.com>
2022-04-26 14:29:20 +0200

    Ajoute header pour les fonctions import et prerempli import.cpp

--- import.cpp
+++ import.cpp
@@ -1,1 +1,9 @@
-ma bite
+#include "import.h"
+
+double distance(point a, point b);
+
+void import_fic_cord(double **t);
+
+void import_fic_matrice(double **t);
+
+void import(std::string s);
---
+++ import.h
@@ -0,0 +1,33 @@
+#include <string>
+
+struct point
+{
+	unsigned int x;
+	unsigned int y;
+};
+
+/*
+ * Cette fonction retourne la distance entre deux points @a et @b
+ * Elle sert a creer une matrice d'adjacence si l'instance passé en paramètre
+ * n'en possède deja pas une
+ */
+double distance(point a, point b);
+
+/*
+ * Cette procedure construit rempli une matrice d'adjacence
+ * dynamique avec la fonction distance
+ */
+void import_fic_cord(double **t);
+
+/*
+ * Cette procedure rempli une matrice d'adjacence dynamique avec les données
+ * fouries par l'instance à resoudre
+ */
+void import_fic_matrice(double **t);
+
+/*
+ * Cette procedure determine quelle type d'instance est passé en paramètre
+ * Soit une instance avec une matrice d'adjacence déja fournie ou une
+ * instance de coordonées pour ensuite appeler la fonction d'import aproprié
+ */
+void import(std::string s);
