3d39772f231caf3586aac1fe9b6d7b65bec7247a
leg7 <leonard.gomez@protonmail.com>
2022-04-26 15:32:21 +0200

    Rajoute ; manquant a import.cpp

--- import.cpp
+++ import.cpp
@@ -4,7 +4,7 @@
 
 double distance(point a, point b)
 {
-    return sqrt( pow(b.y - a.y , 2) + pow(b.x - a.x , 2))
+    return sqrt( pow(b.y - a.y , 2) + pow(b.x - a.x , 2));
 }
 
 void import_fic_cord(double **t, std::string file);
