dc92cb6f42629fa8d8494129fd26b0717faf5500
leg7 <leonard.gomez@protonmail.com>
2022-05-01 18:05:06 +0200

    Mettre a jour les commentaires de calcul-itineraire.h

--- calcul-itineraire.h
+++ calcul-itineraire.h
@@ -8,10 +8,21 @@ struct destination
 
 struct itinerary
 {
-	destination *data; // Un tableau de destination (e.g un itineraire)
-	double length; // La longeure de l'itineraire
+	destination *data;  // Un tableau de destination (e.g un itineraire)
+	double length;      // La longeure de l'itineraire
 };
 
+/*
+ * Cette fonction retourne la destination la plus proche de la ville passé en
+ * paramètre @line dans la matrice d'adjacence @tsp. De plus chaque possibilité
+ * de la ville est mise a zero pour ne pas pouvoir revenir à cette ville
+ * Cette fonction sert a calculer un itineraire de façon gloutonne
+ */
 destination get_destination(matrix &tsp, unsigned int line);
 
-void glouton(matrix &tsp, itinerary &i);
+/*
+ * Cette procedure calcule un itineraire de manière gloutonne grâce a la
+ * fonction get_destination()
+ * L'itineraire @it est calcule à partir d'une matrice d'adjacence @tsp
+ */
+void glouton(matrix &tsp, itinerary &it);
