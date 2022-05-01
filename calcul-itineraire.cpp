0223160e08025db7840a2527297b1051e8c742e3
leg7 <leonard.gomez@protonmail.com>
2022-05-01 12:34:04 +0200

    Réparé bug dernière ligne dans get_min_and_zero()
    Comme la dernière destination n'est pas présente dans la matrice
    d'adjacence il faut prendre en compte ce cas dans get_min_and_zero.

--- calcul-itineraire.cpp
+++ calcul-itineraire.cpp
@@ -1,52 +1,60 @@
 #include "import.h"
 #include "calcul-itineraire.h"
+#include <iostream>
 
 unsigned int get_min_and_zero(matrix &tsp, unsigned int line)
 {
 	unsigned int lmin = 1000000;
 	unsigned int min = 1000000;
 
+	bool exception = false;
+	if (line == tsp.n - 1)
+		exception = true;
+
 	/* init */
 	unsigned int i = line;
 	unsigned int j = 0;
-	if (tsp.m[i][j] != 0)
+
+	if (tsp.m[i][j] != 0 and not exception)
 	{
 		min = tsp.m[i][j];
 		lmin = line + 1;
 		tsp.m[i][j] = 0;
-		--i;
 	}
-	else
-		--i;
+	--i;
+
 
 	/* min diagonale */
-	while (true and line != 0)
+	if (line != 0)
 	{
-		if (tsp.m[i][j] < min and tsp.m[i][j] != 0)
+		while (i != 0)
 		{
-			min = tsp.m[i][j];
-			lmin = i;
-		}
-		tsp.m[i][j] = 0;
+			if (tsp.m[i][j] < min and tsp.m[i][j] != 0)
+			{
+				min = tsp.m[i][j];
+				lmin = i;
+			}
+			tsp.m[i][j] = 0;
 
-		if (i == 0)
-			break;
-		else
-		{
-			--i;
-			++j;
+			if (i != 0)
+			{
+				--i;
+				++j;
+			}
 		}
 	}
 
-	/* min line (et donc du tout) */
-	for (unsigned int z = 1; z < tsp.n - 1 - line; ++z)
+		/* min line (et donc du tout) */
+	unsigned int k = 1;
+	while (k < tsp.n - 1 - line and not exception)
 	{
-		if (tsp.m[line][z] < min and tsp.m[line][z] != 0)
+		if (tsp.m[line][k] < min and tsp.m[line][k] != 0)
 		{
-			min = tsp.m[line][z];
-			lmin = line + z + 1;
+			min = tsp.m[line][k];
+			lmin = line + k + 1;
 		}
-		tsp.m[line][z] = 0;
+		tsp.m[line][k] = 0;
+		++k;
 	}
 
 	return lmin;
@@ -57,9 +65,11 @@ unsigned int* glouton(matrix &tsp)
 	unsigned int *t = new unsigned int[tsp.n];
 	t[0] = 0;
 
-	for (unsigned int i = 1; i < tsp.n - 1; ++i)
+	for (unsigned int i = 1; i < tsp.n; ++i)
 	{
 		t[i] = get_min_and_zero(tsp,t[i-1]);
+		/* print_matrix(tsp); */
+		/* std::cout << t[i] << std::endl << std::endl; */
 	}
 	return t;
 }
