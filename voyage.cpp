2e91b6524f5a3d6cb06ee9b76cf0abd315215d2b
galex <alex.gelineau49@gmail.com>
2022-05-05 11:25:08 +0200

    Ajouter demande pour faire .gif

--- voyage.cpp
+++ voyage.cpp
@@ -3,6 +3,7 @@
 #include "itineraire.h"
 #include "calcul-itineraire.h"
 
+#include <bits/stdc++.h>
 #include <string>
 
 int main()
@@ -19,6 +20,29 @@ int main()
 
 	i = find_best_optimized_greedy_itinerary(tsp, instance);
 	print_itinerary(i);
+
+	char gnuplot;
+	std::cout << "\tVoulez-vous créer un .gif des résultats ? (y/N) : ";
+	std::cin >> gnuplot;
+
+	switch (gnuplot)
+	{
+	case 'y':
+	case 'Y':
+	{
+		std::string com = "gnuplot -e \"filename = \\\"./" + instance + ".dat\\\"\" ./tsp/plot.gp  2>/dev/null";
+		const char *command = com.c_str();
+		std::cout << "\tCréation du fichier .gif..." << std::endl;
+		system(command);
+		break;
+	}
+	case 'n':
+	case 'N':
+		std::cout << "\tLe fichier .gif ne sera pas créé" << std::endl;
+		break;
+	default:
+		std::cout << "\tJe n'ai pas compris !" << std::endl;
+	}
 
 	delete[] i.data;
 	delete_matrix(tsp);
